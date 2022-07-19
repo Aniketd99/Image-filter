fddabd409a8f641f1ad4e6b7564684445577fcf0
Aniketd99 <93241051+Aniketd99@users.noreply.github.com>
2022-07-19 13:00:00 +0530

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,9 @@
+import cv2
+from instafilter import Instafilter
+
+model = Instafilter("Lo-fi")
+new_image = model("image.jpeg")
+
+# To save the image, use cv2
+import cv2
+cv2.imwrite("modified_image.jpg", new_image)
